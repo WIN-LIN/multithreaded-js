4ca37f50f1fcdb924e5fbc2190dc956e88983e5e
whitney.chou <whitney.chou@linecorp.com>
2024-01-27 21:32:56 +0800

    ✨ Shared workers

---
+++ ch2-shared-workers/red.js
@@ -0,0 +1,11 @@
+console.log("red.js");
+
+const worker = new SharedWorker("shared-worker.js");
+
+worker.port.onmessage = (event) => {
+  console.log("EVENT", event.data);
+};
+
+window.addEventListener("beforeunload", () => {
+  worker.port.postMessage("close");
+});
---
+++ ch2-shared-workers/shared-worker.js
@@ -0,0 +1,23 @@
+const ID = Math.floor(Math.random() * 999999);
+
+console.log("shared-worker.js", ID);
+
+const ports = new Set();
+
+self.onconnect = (event) => {
+  const port = event.ports[0];
+  ports.add(port);
+  console.log("CONN", ID, ports.size);
+
+  port.onmessage = (event) => {
+    if (event.data === "close") {
+      ports.delete(port);
+      return;
+    }
+
+    console.log("MESSAGE", ID, event.data);
+    for (let p of ports) {
+      p.postMessage([ID, event.data]);
+    }
+  };
+};
